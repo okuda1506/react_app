3d6f9acaa485e5f2c4f4c164fee73c35f10b1f73
okuda1506 <okuda1506@gmail.com>
2023-05-02 10:15:29 +0900

    イベントのバインド

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import './App.css'
 import Rect from './Rect'
 
 class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = {
+			counter: 0,
+			msg: 'count start!',
+		}
+		this.doAction = this.doAction.bind(this)
+	}
+
+	doAction(event){
+		this.setState({
+			counter: this.state.counter + 1,
+			msg: '*** count: ' + this.state.counter + ' ***'
+		})
+	}
+
 	render(){
 		return  <div>
 			<h1 className="bg-primary text-white display-4">React</h1>  
 			<div className="container">
-				<p className="subtitle">draw rectangle.</p>
-				<Rect x="200" y="200" w="200" h="200" c="#6ff9" r="25" />
-				<Rect x="300" y="300" w="200" h="200" c="#f6f9" r="75"/>
-				<Rect x="400" y="400" w="200" h="200" c="#6669" r="100" />
+				<p className="subtitle">Count number.</p>
+				<div className="alert alert-primary text-center">
+				<p className="h5 mb-4">{this.state.msg}</p>
+				<button className="btn btn-primary" 
+					onClick={this.doAction}>
+					Click</button>
+				</div>
 			</div>
 		</div>
 	}
