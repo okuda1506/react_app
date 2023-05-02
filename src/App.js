9cefb7e44d3b8195da0b1223668e6db8ef1ab614
okuda1506 <okuda1506@gmail.com>
2023-05-02 11:59:22 +0900

    クリックすると四角が現れてくるやつ

--- src/App.js
+++ src/App.js
@@ -3,32 +3,51 @@ import './App.css'
 import Rect from './Rect'
 
 class App extends Component {
+	data = []
+
+	area = {
+		width:"500px",
+		height:"500px",
+		border:"1px solid blue"
+	}
+	
 	constructor(props){
 		super(props);
-		this.state = {
-			counter: 0,
-			msg: 'count start!',
+		this.state ={
+			list: this.data
 		}
 		this.doAction = this.doAction.bind(this)
 	}
 
-	doAction(event){
+	doAction(e){
+		let x = e.pageX
+		let y = e.pageY
+		this.data.push({x:x, y:y})
 		this.setState({
-			counter: this.state.counter + 1,
-			msg: '*** count: ' + this.state.counter + ' ***'
+			list:this.data
 		})
+	}
+
+	draw(d){
+		let s = {
+			position:"absolute",
+			left:(d.x - 25) + "px",
+			top:(d.y - 25) + "px",
+			width:"50px",
+			height:"50px",
+			backgroundColor:"#66f3",
+		}
+		
+		return <div style={s}></div>
 	}
 
 	render(){
 		return  <div>
 			<h1 className="bg-primary text-white display-4">React</h1>  
 			<div className="container">
-				<p className="subtitle">Count number.</p>
-				<div className="alert alert-primary text-center">
-				<p className="h5 mb-4">{this.state.msg}</p>
-				<button className="btn btn-primary" 
-					onClick={this.doAction}>
-					Click</button>
+				<p className="subtitle">draw rectangle.</p>
+				<div style={this.area} onClick={this.doAction}>
+					{this.data.map((value)=>this.draw(value))}
 				</div>
 			</div>
 		</div>
