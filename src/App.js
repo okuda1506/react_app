4197b6f379d214dd6368d04fac7a2958ff08d242
okuda1506 <okuda1506@gmail.com>
2023-05-03 16:22:22 +0900

    コンテキストの利用

--- src/App.js
+++ src/App.js
@@ -2,54 +2,46 @@ import React, { Component } from 'react'
 import './App.css'
 import Rect from './Rect'
 
+let data = {
+	title: 'React-Context',
+	message: 'this is sample message.'
+}
+
+const SampleContext = React.createContext(data)
+
 class App extends Component {
-	data = []
+	render(){
+	return <div>
+		<h1 className="bg-primary text-white display-4">React</h1>
+		<div className="container">
+			<Title />
+			<Message />
+		</div>
+	</div>
+	}
+}
 
-	area = {
-		width:"500px",
-		height:"500px",
-		border:"1px solid blue"
-	}
-	
-	constructor(props){
-		super(props);
-		this.state ={
-			title: 'input form',
-			message: 'type your name.'
-		}
-		this.doChange = this.doChange.bind(this)
-		this.doSubmit = this.doSubmit.bind(this)
-	}
-
-	doChange(event){
-		this.input = event.target.value
-	}
-
-	doSubmit(event){
-		this.setState({
-			title: 'send form',
-			message: 'Hello, ' + this.input + '!!'
-		})
-		event.preventDefault()
-	}
+class Title extends Component {
+	static contextType = SampleContext
 
 	render(){
-		return <div>
-			<h1 className="bg-primary text-white display-4">React</h1>
-			<div className="container">
-				<h4>{this.state.title}</h4>
-				<p className="card h5 p-3">{this.state.message}</p>
-				<div className="alert alert-primary mt-3">
-					<form onSubmit={this.doSubmit}>
-						<div className="form-group">
-							<label>Message:</label>
-							<input type="text" className="form-control" onChange={this.doChange} required pattern="[A-Za-z _,.]+"/>
-						</div>
-						<input type="submit" className="btn btn-primary" value="Click" />
-					</form>
-				</div>
+		return (
+			<div className="card p-2 my-3">
+				<h2>{this.context.title}</h2>
 			</div>
-		</div>
+		)
+	}
+}
+
+class Message extends Component {
+	static contextType = SampleContext
+	
+	render(){
+		return (
+			<div className="alert alert-primary">
+				<p>{this.context.message}</p>
+			</div>
+		)
 	}
 }
 
