bde6ae19b4912473c20500e35b8ec2a41a274b9a
okuda1506 <okuda1506@gmail.com>
2023-05-05 16:01:38 +0900

    ステートで数字をカウントする

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import './App.css'
-import Rect from './Rect'
 
-class App extends Component {
-	data = []
-
-	area = {
-		width:"500px",
-		height:"500px",
-		border:"1px solid blue"
-	}
-	
-	constructor(props){
-		super(props);
-		this.state ={
-			list: this.data
-		}
-		this.doAction = this.doAction.bind(this)
+function App() {
+	const [count, setCount] = useState(0)
+	const clickFunc = () => {
+		setCount(count + 1)
 	}
 
-	doAction(e){
-		let x = e.pageX
-		let y = e.pageY
-		this.data.push({x:x, y:y})
-		this.setState({
-			list:this.data
-		})
-	}
-
-	draw(d){
-		let s = {
-			position:"absolute",
-			left:(d.x - 25) + "px",
-			top:(d.y - 25) + "px",
-			width:"50px",
-			height:"50px",
-			backgroundColor:"#66f3",
-		}
-		
-		return <div style={s}></div>
-	}
-
-	render(){
-		return  <div>
-			<h1 className="bg-primary text-white display-4">React</h1>  
+	return (
+		<div>
+			<h1 className="bg-primary text-white display-4 ">React</h1>
 			<div className="container">
-				<p className="subtitle">draw rectangle.</p>
-				<div style={this.area} onClick={this.doAction}>
-					{this.data.map((value)=>this.draw(value))}
+				<h4 className="my-3">Hooks sample</h4>
+				<div className="alert alert-primary text-center">
+					<p className="h5 mb-3">click: {count} times!</p>
+					<div>
+						<button className="btn btn-primary"
+							onClick={clickFunc}>
+							Click me
+						</button>
+					</div>
 				</div>
 			</div>
 		</div>
-	}
+	)
 }
 
 export default App
