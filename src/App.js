c5f97d04c390ff0feb2c1f60f5fbb8b3f1fa9da6
okuda1506 <okuda1506@gmail.com>
2023-05-07 14:23:04 +0900

    副作用フックで1から入力値までの合計を出力

--- src/App.js
+++ src/App.js
@@ -1,29 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 
+function AlertMessage(props) {
+	return  <div className="alert alert-primary h5 text-primary">
+		<h5>{props.msg}</h5>
+	</div>
+}
+
 function App() {
-	const [count, setCount] = useState(0)
-	const clickFunc = () => {
-		setCount(count + 1)
+	const [val, setVal] = useState(0)
+	const [msg, setMsg] = useState("set a number...")
+	
+	const doChange = (event) => {
+		setVal(event.target.value)
 	}
+	
+	useEffect(() => {
+		let total = 0
+		for (let i = 0; i <= val; i++) {
+			total += i
+		}
+		setMsg("total: " + total + ".")
+	})
 
 	return (
 		<div>
 			<h1 className="bg-primary text-white display-4 ">React</h1>
 			<div className="container">
 				<h4 className="my-3">Hooks sample</h4>
-				<div className="alert alert-primary text-center">
-					<p className="h5 mb-3">click: {count} times!</p>
-					<div>
-						<button className="btn btn-primary"
-							onClick={clickFunc}>
-							Click me
-						</button>
-					</div>
+				<AlertMessage msg={msg} />
+				<div className="form-group">
+					<label>Input:</label>
+					<input type="number" className="form-control" 
+						onChange={doChange} />
 				</div>
 			</div>
 		</div>
-	)
+  	)
 }
 
 export default App
